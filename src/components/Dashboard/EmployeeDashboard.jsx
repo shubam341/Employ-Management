54fb285657ccaaffe3a3a32bf9da0a7a5efc6731
shubam <chshivam148@gmail.com>
2025-02-05 22:42:19 +0530

    Task lsit added

--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,11 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import Header from '../others/Header';
+import TaskList from '../others/TaskList';
 
 const EmployeeDashboard=()=>{
     return(
         <div className='p-15 bg-[#1C1C1C] h-screen'>
             <Header/>
+            <TaskList/>
         </div>
     )
 }
---
+++ src/components/others/TaskList.jsx
@@ -0,0 +1,32 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+
+const TaskList=()=>{
+    return(
+        <div className='flex mt-10  justify-between gap-5 screen'>
+<div className=' text-white rounded-xl w-[45%]  py-6 px-9 bg-red-400'>
+   <h2 className='text-3xl font-semibold'>0</h2>
+   <h3 className='text-xl font medium'>New Task</h3>
+</div>
+ 
+<div className=' text-white rounded-xl w-[45%]  py-6 px-9 bg-blue-400'>
+   <h2 className='text-3xl font-semibold'>0</h2>
+   <h3 className='text-xl font medium'>New Task</h3>
+</div>
+
+<div className=' text-white rounded-xl w-[45%]  py-6 px-9 bg-green-400'>
+   <h2 className='text-3xl font-semibold'>0</h2>
+   <h3 className='text-xl font medium'>New Task</h3>
+</div>
+
+<div className=' text-white rounded-xl w-[45%]  py-6 px-9 bg-yellow-400'>
+   <h2 className='text-3xl font-semibold'>0</h2>
+   <h3 className='text-xl font medium'>New Task</h3>
+</div>
+
+        </div>
+    )
+}
+
+export default TaskList
