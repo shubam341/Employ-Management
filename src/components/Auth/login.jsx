890bca00465b1fc37dffadea8a1ce3117bb725fe
shubam <chshivam148@gmail.com>
2025-02-05 13:55:38 +0530

    User Login setup

--- src/components/Auth/login.jsx
+++ src/components/Auth/login.jsx
@@ -1,14 +1,38 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login=()=>{
+  
+      const [email,setEmail]=useState('')
+      const [password,setPassword]=useState('')
+    
+      const submitHandler=(e)=>{
+        e.preventDefault()
+   console.log("email is",email)
+   console.log("passwordl is",password)
+    }
     return ( 
    <div className='flex h-screen w-screen items-center justify-center'>
-    <div className='border-3 border-red-600 p-200'>
-        <form  className='flex flex-col items-center justify-center'>
-            <input  className='text-black outline-none bg-transparent border-2 border-emerald-600 text-xl py-10 px-4  rounded-full placeholder:text-white'type="email" placeholder='Enter your email' />
-            <input className='text-black outline-none bg-transparent border-2  border-emerald-600 text-xl py-4 px-5  rounded-full mt-5 placeholder:text-white' type="password" placeholder ='Enter password' />
-            <button className='text-white border-none outline-none border-2  bg-emerald-600 text-xl py-4 px-5  rounded-full mt-10 placeholder:text-white'>Login</button>
+    <div className='border-2 rounded-xl border-emerald-600 p-20'>
+        
+        <form 
+        onSubmit={(e)=>{
+           submitHandler(e)
+        }}
+        className='flex flex-col items-center justify-center'>
+            <input
+            value={email}
+            onChange={(e)=>{
+                setEmail(e.target.value)
+            }}
+             required className='text-white outline-none bg-transparent border-2 border-emerald-600 text-xl py-3 px-5 rounded-full placeholder:text-gray-400'type="email" placeholder='Enter your email' />
+            <input 
+            value={password}
+            onChange={(e)=>{
+                setPassword(e.target.value)
+            }}
+            required className=' text-white outline-none bg-transparent border-2  border-emerald-600 text-xl py-3 px-5  rounded-full mt-3 placeholder:text-gray-400' type="password" placeholder ='Enter password' />
+            <button className='mt-5 text-white border-none outline-none border-2  bg-emerald-600 text-xl py-3 px-5  rounded-full  placeholder:text-white'>Login</button>
         </form>
     </div>
    </div>
