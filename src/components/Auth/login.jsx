5abdc9fabf2499706e15f0d4e483e31499483b0e
shubam <chshivam148@gmail.com>
2025-02-05 12:17:11 +0530

    Changed Bordee

--- src/components/Auth/login.jsx
+++ src/components/Auth/login.jsx
@@ -6,9 +6,9 @@ const Login=()=>{
    <div className='flex h-screen width-screen items-center justify-center'>
     <div className='border-3 border-red-600'>
         <form  className='flex flex-col items-center justify-center'>
-            <input  className='text-black outline-none bg-transparent border-2 border-emerald-600 text-xl py-4 px-5  rounded-full placeholder:text-white'type="email" placeholder='Enter your email' />
-            <input className='text-black outline-none bg-transparent border-2 border-emerald-600 text-xl py-4 px-5  rounded-full placeholder:text-white' type="passwrd" placeholder ='Enter password' />
-            <button>Login</button>
+            <input  className='text-black outline-none bg-transparent border-2 border-emerald-600 text-xl py-10 px-4  rounded-full placeholder:text-white'type="email" placeholder='Enter your email' />
+            <input className='text-black outline-none bg-transparent border-2  border-emerald-600 text-xl py-4 px-5  rounded-full mt-5 placeholder:text-white' type="passwrd" placeholder ='Enter password' />
+            <button className='text-white border-none outline-none border-2  bg-emerald-600 text-xl py-4 px-5  rounded-full mt-10 placeholder:text-white'>Login</button>
         </form>
     </div>
    </div>
