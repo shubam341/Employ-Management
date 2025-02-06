7b6cf2d606a5aabb89644b6f328c40cfebcef9e4
shubam <chshivam148@gmail.com>
2025-02-06 13:52:46 +0530

    Login credentials completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useEffect, useState } from 'react';
-// import AdminDashboard from './components/Dashboard/AdminDashboard';
-// import EmployeeDashboard from './components/Dashboard/EmployeeDashboard';
+import AdminDashboard from './components/Dashboard/AdminDashboard';
+import EmployeeDashboard from './components/Dashboard/EmployeeDashboard';
 import Login from './components/Auth/login.jsx'
 
 // import { setLocalStorage,getLocalStorage } from './utils/localstorage.jsx';
@@ -22,9 +22,11 @@ const [user,setuser]=useState(null)
 
 const handleLogin=(email,password)=>{
     if(email=='admin@gmail.com'&& password=='123'){
-console.log("This is admin")
-}else if(email=='user@megmail.com'&& password=='123'){
-    console.log("This is User")
+     setuser('admin')
+     console.log(user)
+}else if(email=='user@gmail.com'&& password=='123'){
+    setuser('employee')
+    console.log(user)
 }
 else{
     alert("Invalid Credentials")
@@ -37,8 +39,8 @@ else{
 
     return (     <>
       {!user ? <Login handleLogin={handleLogin}/>: ''}
-    {/* <EmployeeDashboard/> */}
-    {/* <AdminDashboard/> */}
+     {user =='admin'?<AdminDashboard/>:<EmployeeDashboard/>}
+  
     </>
     )
 }
